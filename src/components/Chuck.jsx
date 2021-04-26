dc2c578e8df49405e2bf3ec4748f5ed5e2d9f53f
Nulledcore <nulledcoreofficial@gmail.com>
2021-04-26 10:34:03 +0200

    added material-ui cuz it's cool B-)

--- src/components/Chuck.jsx
+++ src/components/Chuck.jsx
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
+import { ThemeProvider, createMuiTheme, CssBaseline } from '@material-ui/core';
+import red from '@material-ui/core/colors/red';
+
+const theme = createMuiTheme({
+    palette: {
+      type: "dark",
+      primary: red,
+      secondary: {
+        main: '#5c6bc0',
+      },
+    },
+});
+
 class Chuck extends Component {
     state = {  }
     render() { 
-        return ( <div><p>Hello World from Chuck</p> </div>);
+        return (
+        <ThemeProvider theme={theme}>
+            <div>
+                <p>Hello World from Chuck</p>
+            </div>
+            <CssBaseline/>
+        </ThemeProvider>
+        );
     }
 }
  
